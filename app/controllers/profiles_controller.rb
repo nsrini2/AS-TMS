06f2a4350e02f25f2d71931c51adbb1ebd9f7be1
markm <markm@e05bbbe3-11a3-40ec-a18c-f20a9e1084ec>
2010-10-13 18:24:41 +0000

    Implement a lot of the answered questions on the hub
    git-svn-id: http://cubeless.dev.sabre.com:18080/svn/cubeless/communities/agentstream@18807 e05bbbe3-11a3-40ec-a18c-f20a9e1084ec

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -18,7 +18,18 @@ class ProfilesController
     @profile_stats[:blog_posts] = current_profile.blog_posts.count
     
     @questions_referred_to_me = current_profile.questions_referred_to_me
+    
+    # TODO: Organized by the last ones I participated in
     @groups = current_profile.groups
+    @groups = @groups[@groups.size-3..@groups.size-1].reverse
+
+
+    # TODO: Clean this up...scraped from ExplorationsController.
+    # Special options needed to ensure only personal posts or group posts that I can access show up
+    blog_options = blog_filters
+    ModelUtil.add_joins!(blog_options,"left join groups pg on pg.id = blogs.owner_id and blogs.owner_type = 'Group' and pg.group_type = 2")
+    ModelUtil.add_conditions!(blog_options,"pg.id is null")
+    @blog_posts = BlogPost.find(:all, blog_options)
 
     # TODO: Make into one query
     @messages = [current_profile.notes, current_profile.sent_notes].flatten.sort_by(&:created_at).reverse
