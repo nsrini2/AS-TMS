9c32919959bd8d4a2678b5b5a40c86d51465a8ed
markm <markm@e05bbbe3-11a3-40ec-a18c-f20a9e1084ec>
2010-11-01 22:23:44 +0000

    Move to an even cleaner public/private layout recognition
    git-svn-id: http://cubeless.dev.sabre.com:18080/svn/cubeless/communities/agentstream@18843 e05bbbe3-11a3-40ec-a18c-f20a9e1084ec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,28 +6,19 @@ class ApplicationController
   
   helper :all
   
+  layout :public_or_private_layout
+  
   def active_users
     @active_users ||= Profile.active_users_count
   end
   
-  private
-  def render_appropriate_layout
-    if !current_profile
-      render :layout => 'public' and return
-    end    
-  end
-  
+  private  
   def public_or_private_layout
     if !current_profile
       'public'
     else
-      ApplicationController.read_inheritable_attribute(:layout)
+      '_application'
     end
   end
   
-end
-
-# Check certain controllers for public/private layout options
-%w(account display feedback retrievals).each do |name|
-  "#{name.titlecase}Controller".constantize.__send__ :layout, :public_or_private_layout
 end
