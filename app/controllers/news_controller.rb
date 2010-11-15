1ba76d94f250d4e23f866add1345367375c5bc34
markm <markm@e05bbbe3-11a3-40ec-a18c-f20a9e1084ec>
2010-11-15 07:49:31 +0000

    Actually use the tnooz id instead of the test one
    git-svn-id: http://cubeless.dev.sabre.com:18080/svn/cubeless/communities/agentstream@18893 e05bbbe3-11a3-40ec-a18c-f20a9e1084ec

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,7 +2,7 @@ class NewsController < ApplicationController
   
   def index
     begin
-      @news = Group.find(16).blog.blog_posts.recent
+      @news = Group.find(83).blog.blog_posts.recent
     rescue
       @news = []
     end
