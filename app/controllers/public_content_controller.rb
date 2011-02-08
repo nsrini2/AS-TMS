6b8b5792d3c79443eefb07e5aa6d0e094bc5f82c
markm <markm@e05bbbe3-11a3-40ec-a18c-f20a9e1084ec>
2011-02-08 02:40:59 +0000

    Add formtastic html to /refer-a-friend
    git-svn-id: http://cubeless.dev.sabre.com:18080/svn/cubeless/communities/agentstream@19066 e05bbbe3-11a3-40ec-a18c-f20a9e1084ec

--- app/controllers/public_content_controller.rb
+++ app/controllers/public_content_controller.rb
@@ -3,7 +3,6 @@ class PublicContentController < ApplicationController
   layout "public"
   
   skip_before_filter :require_auth
-  #skip_before_filter :require_terms_acceptance
   
   def refer_a_friend
     
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,10 @@ ActionController::Routing::Routes.draw do |map|
   
   map.news '/news', :controller => 'news', :action => 'index'
   
+  # Refer a friend campaign
   map.connect '/refer_a_friend', :controller => 'public_content', :action => 'refer_a_friend'
+  map.connect '/refer-a-friend', :controller => 'public_content', :action => 'refer_a_friend'
+  map.connect '/referafriend', :controller => 'public_content', :action => 'refer_a_friend'
 
   # Sample of regular route:
   #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
