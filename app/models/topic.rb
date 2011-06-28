2728a12ac26e9336f9435cdb5147d1de6fe009de
scottj <scottj@e05bbbe3-11a3-40ec-a18c-f20a9e1084ec>
2011-06-28 20:32:46 +0000

    remove indices for chats that are inactive -- add them back if activated
    git-svn-id: http://cubeless.dev.sabre.com:18080/svn/cubeless/communities/agentstream@19295 e05bbbe3-11a3-40ec-a18c-f20a9e1084ec

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,9 +3,12 @@ require "#{Rails.root}/vendor/plugins/live_qa/app/models/topic"
 class Topic
 
   def after_save
-    unless self.chat && self.chat.on_air?
+    #if the chat is inactive, Topic won't find it
+    if self.chat  
       # process the indexing in dalay job
-      ChatTopicIndex.delay.update_indices(self)
+      ChatTopicIndex.delay.update_indices(self) unless self.chat.on_air?
+    else
+      chat_topic_index.delay.destroy  
     end
   end
   
