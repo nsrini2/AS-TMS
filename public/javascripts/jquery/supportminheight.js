a8c6a36957fe5c2b2e5055ccb332f36ed0b1e8d8
scottj <scottj@e05bbbe3-11a3-40ec-a18c-f20a9e1084ec>
2012-01-30 20:12:27 +0000

    removed unwanted jquery files
    git-svn-id: http://cubeless.dev.sabre.com:18080/svn/cubeless/communities/agentstream@19844 e05bbbe3-11a3-40ec-a18c-f20a9e1084ec

--- public/javascripts/jquery/supportminheight.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function($) {
-	var $div = $('<div style="height: 1px; min-height: 2px; position: absolute; top: -100px; left: -100px;"/>').appendTo('body');
-	$.support.minHeight = !!( $div[0].offsetHeight && $div[0].offsetHeight == 2 );
-	$div.remove();
-});
