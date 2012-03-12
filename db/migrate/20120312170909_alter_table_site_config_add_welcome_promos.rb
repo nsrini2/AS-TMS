7663d6ef7a9a4e939e5851860f99205ea7113c67
scottj <scottj@e05bbbe3-11a3-40ec-a18c-f20a9e1084ec>
2012-03-12 20:46:48 +0000

    set default values for promo messages
    git-svn-id: http://cubeless.dev.sabre.com:18080/svn/cubeless/communities/agentstream@19874 e05bbbe3-11a3-40ec-a18c-f20a9e1084ec

--- db/migrate/20120312170909_alter_table_site_config_add_welcome_promos.rb
+++ db/migrate/20120312170909_alter_table_site_config_add_welcome_promos.rb
@@ -1,11 +1,11 @@
 class AlterTableSiteConfigAddWelcomePromos < ActiveRecord::Migration
   def self.up
-    add_column :site_configs, :welcome_promo_title_1, :string
-    add_column :site_configs, :welcome_promo_1, :string
-    add_column :site_configs, :welcome_promo_title_2, :string
-    add_column :site_configs, :welcome_promo_2, :string
-    add_column :site_configs, :welcome_promo_title_3, :string
-    add_column :site_configs, :welcome_promo_3, :string
+    add_column :site_configs, :welcome_promo_title_1, :string, :default => ""
+    add_column :site_configs, :welcome_promo_1, :string, :default => ""
+    add_column :site_configs, :welcome_promo_title_2, :string, :default => ""
+    add_column :site_configs, :welcome_promo_2, :string, :default => ""
+    add_column :site_configs, :welcome_promo_title_3, :string, :default => ""
+    add_column :site_configs, :welcome_promo_3, :string, :default => ""
   end
 
   def self.down
