2c09f653779c24be54bc97b26bb925133117a252
scottj <scottj@e05bbbe3-11a3-40ec-a18c-f20a9e1084ec>
2012-04-11 21:42:22 +0000

    have deals not load by default on the D&E page.  dont load tracking we are not using
    git-svn-id: http://cubeless.dev.sabre.com:18080/svn/cubeless/communities/agentstream@19894 e05bbbe3-11a3-40ec-a18c-f20a9e1084ec

--- vendor/gems/deals_and_extras-0.0.2/app/models/offer_type.rb
+++ vendor/gems/deals_and_extras-0.0.2/app/models/offer_type.rb
@@ -1,6 +1,10 @@
 class OfferType < ActiveRecord::Base
 
   has_many :offers
+  
+  def html_checked
+    true unless self.offer_type.match(/^deals$/i)
+  end
 
   def self.OPTIONIZE
     self.all.collect{|t| [t.offer_type, t.id] }
