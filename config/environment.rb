b963b76c74435cee06083869a4d05d488988b2a4
scottj <scottj@e05bbbe3-11a3-40ec-a18c-f20a9e1084ec>
2012-07-31 21:18:26 +0000

    revert Gemfile to specify Rack version
    git-svn-id: http://cubeless.dev.sabre.com:18080/svn/cubeless/communities/agentstream@19987 e05bbbe3-11a3-40ec-a18c-f20a9e1084ec

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# Load the correct version of Rack
-Config.gem "rack", :version=>'1.2.5'
-
 # Initialize the rails application
 AgentStream::Application.initialize!
