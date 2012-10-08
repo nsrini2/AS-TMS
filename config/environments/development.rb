a4b144685dbce6f598fa28ba8f675ffab4e8d042
scottj <scottj@e05bbbe3-11a3-40ec-a18c-f20a9e1084ec>
2012-10-08 19:51:59 +0000

    update blog posts to use new helpful/unhelpful rating system
    git-svn-id: http://cubeless.dev.sabre.com:18080/svn/cubeless/communities/agentstream@20041 e05bbbe3-11a3-40ec-a18c-f20a9e1084ec

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,8 +5,8 @@ AgentStream::Application.configure do
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the webserver when you make code changes.
   # -- This needs to be off if you are monkeypunching a library, so it does not get re-punched
-  # config.cache_classes = false
-  config.cache_classes = true
+  config.cache_classes = false
+  # config.cache_classes = true
 
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
