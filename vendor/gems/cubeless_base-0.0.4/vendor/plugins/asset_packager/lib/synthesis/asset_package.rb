dda7637e862e1ddd4078d3a032c64d8c47652cc3
scottj <scottj@e05bbbe3-11a3-40ec-a18c-f20a9e1084ec>
2012-10-17 18:26:01 +0000

    fix other dependency deprication waring in asset_packager
    git-svn-id: http://cubeless.dev.sabre.com:18080/svn/cubeless/communities/agentstream@20054 e05bbbe3-11a3-40ec-a18c-f20a9e1084ec

--- vendor/gems/cubeless_base-0.0.4/vendor/plugins/asset_packager/lib/synthesis/asset_package.rb
+++ vendor/gems/cubeless_base-0.0.4/vendor/plugins/asset_packager/lib/synthesis/asset_package.rb
@@ -3,7 +3,7 @@ module Synthesis
 
     # class variables
     @@asset_packages_yml = $asset_packages_yml ||
-      (File.exists?("#{Rails.root}/config/asset_packages.yml") ? YAML.load_file("#{RAILS_ROOT}/config/asset_packages.yml") : nil) ||
+      (File.exists?("#{Rails.root}/config/asset_packages.yml") ? YAML.load_file("#{Rails.root}/config/asset_packages.yml") : nil) ||
       (File.exists?("#{CubelessBase::Engine.root.to_s}/config/asset_packages.yml") ? YAML.load_file("#{CubelessBase::Engine.root.to_s}/config/asset_packages.yml") : nil) #Fix for engine based cubeless
 
     # singleton methods
