295a82620f17110eeac0574922a3055ac3e7feea
scottj <scottj@e05bbbe3-11a3-40ec-a18c-f20a9e1084ec>
2012-10-17 18:31:59 +0000

    fix more dependency deprication waring in asset_packager
    git-svn-id: http://cubeless.dev.sabre.com:18080/svn/cubeless/communities/agentstream@20056 e05bbbe3-11a3-40ec-a18c-f20a9e1084ec

--- vendor/plugins/asset_packager/lib/synthesis/asset_package.rb
+++ vendor/plugins/asset_packager/lib/synthesis/asset_package.rb
@@ -3,7 +3,7 @@ module Synthesis
 
     # class variables
     @@asset_packages_yml = $asset_packages_yml ||
-      (File.exists?("#{RAILS_ROOT}/config/asset_packages.yml") ? YAML.load_file("#{RAILS_ROOT}/config/asset_packages.yml") : nil) ||
+      (File.exists?("#{Rails.root}/config/asset_packages.yml") ? YAML.load_file("#{Rails.root}/config/asset_packages.yml") : nil) ||
       (File.exists?("#{CubelessBase::Engine.root}/config/asset_packages.yml") ? YAML.load_file("#{CubelessBase::Engine.root}/config/asset_packages.yml") : nil) #Fix for engine based cubeless
     
     # singleton methods
@@ -75,13 +75,13 @@ module Synthesis
       end
 
       def create_yml
-        unless File.exists?("#{RAILS_ROOT}/config/asset_packages.yml")
+        unless File.exists?("#{Rails.root}/config/asset_packages.yml")
           asset_yml = Hash.new
 
-          asset_yml['javascripts'] = [{"base" => build_file_list("#{RAILS_ROOT}/public/javascripts", "js")}]
-          asset_yml['stylesheets'] = [{"base" => build_file_list("#{RAILS_ROOT}/public/stylesheets", "css")}]
+          asset_yml['javascripts'] = [{"base" => build_file_list("#{Rails.root}/public/javascripts", "js")}]
+          asset_yml['stylesheets'] = [{"base" => build_file_list("#{Rails.root}/public/stylesheets", "css")}]
 
-          File.open("#{RAILS_ROOT}/config/asset_packages.yml", "w") do |out|
+          File.open("#{Rails.root}/config/asset_packages.yml", "w") do |out|
             YAML.dump(asset_yml, out)
           end
 
@@ -103,7 +103,7 @@ module Synthesis
       @target = target_parts[2].to_s
       @sources = package_hash[package_hash.keys.first]
       @asset_type = asset_type
-      @asset_path = ($asset_base_path ? "#{$asset_base_path}/" : "#{RAILS_ROOT}/public/") +
+      @asset_path = ($asset_base_path ? "#{$asset_base_path}/" : "#{Rails.root}/public/") +
           "#{@asset_type}#{@target_dir.gsub(/^(.+)$/, '/\1')}"
       @extension = get_extension
       @file_name = "#{@target}_packaged.#{@extension}"
@@ -159,8 +159,8 @@ module Synthesis
       end
 
       def compress(type, source)
-        compressor_path = "#{RAILS_ROOT}/vendor/plugins/asset_packager/lib/yuicompressor"
-        tmp_path = "#{RAILS_ROOT}/tmp/#{@target}_packaged"
+        compressor_path = "#{Rails.root}/vendor/plugins/asset_packager/lib/yuicompressor"
+        tmp_path = "#{Rails.root}/tmp/#{@target}_packaged"
 
         # write out to a temp file
         File.open("#{tmp_path}_uncompressed.#{type}", "w") {|f| f.write(source) }
