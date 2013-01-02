ee337be23fa4167c2cabf5dab032bcf9d56afa3d
scottj <scottj@e05bbbe3-11a3-40ec-a18c-f20a9e1084ec>
2013-01-02 16:34:00 +0000

    aff FEATURE prefix to CONSTANTS that represent features
    git-svn-id: http://cubeless.dev.sabre.com:18080/svn/cubeless/communities/agentstream@20104 e05bbbe3-11a3-40ec-a18c-f20a9e1084ec

--- config/initializers/feature_sets.rb
+++ config/initializers/feature_sets.rb
@@ -1,4 +1,4 @@
 feature_set_config = YAML.load(File.read("#{Rails.root}/config/feature_sets.yml"))
 
-ACTIVITY_STREAM_ADS = feature_set_config["activity_stream_ads"]
-ELASTIC_SEARCH = feature_set_config["elastic_search"]
+FEATURE_ACTIVITY_STREAM_ADS = feature_set_config.member?("activity_stream_ads") && feature_set_config["activity_stream_ads"]
+FEATURE_ELASTIC_SEARCH = feature_set_config.member?("elastic_search") && feature_set_config["elastic_search"]
