9a958fdf630b095b53e11b3f5ea91476501fb8a6
scottj <scottj@e05bbbe3-11a3-40ec-a18c-f20a9e1084ec>
2013-01-02 16:34:28 +0000

    get seeds working
    git-svn-id: http://cubeless.dev.sabre.com:18080/svn/cubeless/communities/agentstream@20111 e05bbbe3-11a3-40ec-a18c-f20a9e1084ec

--- db/seeds.de.rb
+++ db/seeds.de.rb
@@ -1,12 +1,9 @@
-# # This file should contain all the record creation needed to seed the database with its default values.
-# # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-# #
-# # Examples:
-# #
-# #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-# #   Mayor.create(:name => 'Daley', :city => cities.first)
-# 
-# # process the example xml
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+
+# ===========  Deals and Extras ===========================================
+
+# process the example xml
 # Supplier.new_from_xml(open('./db/example.xml').read, DataSource.find_by_data_source("Sabre Enterprise Data Warehouse"))
 # Offer.new_from_xml(open('./db/example.xml').read)
 # 
