71936152a2497fc2f1bc3c36a5946bc3258b908f
scottj <scottj@e05bbbe3-11a3-40ec-a18c-f20a9e1084ec>
2013-01-02 16:34:45 +0000

    fix error with ambigious column name active on group referred questions
    git-svn-id: http://cubeless.dev.sabre.com:18080/svn/cubeless/communities/agentstream@20120 e05bbbe3-11a3-40ec-a18c-f20a9e1084ec

--- app/models/group.rb
+++ app/models/group.rb
@@ -7,10 +7,10 @@ class Group < ActiveRecord::Base
   belongs_to :company
   
   has_many :questions_referred_to_me, :through => :referred_questions, :source => :question,
-    :conditions => ['active = 1'], :uniq  => true
+    :conditions => ['question_referrals.active = 1'], :uniq  => true
     
   has_many :active_questions_referred_to_me, :through => :referred_questions, :source => :question,
-    :conditions => ['active = 1 and questions.open_until > ?', Date.today], :uniq  => true
+    :conditions => ['question_referrals.active = 1 and questions.open_until > ?', Date.today], :uniq  => true
     
   def search_content
     tags
