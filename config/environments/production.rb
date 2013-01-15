f0cf8b25d0b44917bb4234da028834589f908a92
scottj <scottj@e05bbbe3-11a3-40ec-a18c-f20a9e1084ec>
2013-01-15 16:24:59 +0000

    add in changes from logging branch
    git-svn-id: http://cubeless.dev.sabre.com:18080/svn/cubeless/communities/agentstream@20145 e05bbbe3-11a3-40ec-a18c-f20a9e1084ec

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,8 +18,8 @@ AgentStream::Application.configure do
   # If you have no front-end server that supports something like X-Sendfile,
   # just comment this out and Rails will serve the files
 
-  # See everything in the log (default is :info)
-  config.log_level = :warn
+  # LOG -- Using log4r -- settings are made in /config/log4r.yml
+  
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
