df2136211484e44fe881e02e24c5479616cec483
scottj <scottj@e05bbbe3-11a3-40ec-a18c-f20a9e1084ec>
2013-02-22 21:31:34 +0000

    Resolved README.md
    git-svn-id: http://cubeless.dev.sabre.com:18080/svn/cubeless/communities/sriww@20195 e05bbbe3-11a3-40ec-a18c-f20a9e1084ec

--- vendor/gems/cubeless_base-0.0.4/app/controllers/statuses_controller.rb
+++ vendor/gems/cubeless_base-0.0.4/app/controllers/statuses_controller.rb
@@ -1,3 +1,5 @@
+require 'will_paginate/array'
+
 class StatusesController < ApplicationController
   
   deny_access_for :all => :sponsor_member
@@ -6,10 +8,13 @@ class StatusesController < ApplicationController
     setup_profile
     
     @statuses = if @profile
+                  Rails.logger.info("Current profile:" + @profile.first_name)
                   @content_for_status_tab = "selected"
-                  Status.by_profile(@profile).find(:all, :page => default_paging)
+                  #Status.by_profile(@profile).find(:all, :page => default_paging)
+                  Status.by_profile(@profile).paginate(:page => params[:page] ||= 1, :per_page => 10)
                 else
-                  Status.find(:all, :order => "created_at DESC", :page => default_paging)
+                  #Status.find(:all, :order => "created_at DESC", :page => default_paging)
+                  Status.recent.paginate(:page => params[:page] ||= 1, :order => "created_at DESC", :per_page => 10)
                 end
     
     if @profile
