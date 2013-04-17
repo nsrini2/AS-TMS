7054d3708c439caaa485520cace7b1deb5107d92
Niranjana Srinivasan <niranjanas@rapidthink.com>
2013-04-17 12:26:23 +0530

    Removing booth video stuff...again

--- spec/factories/booth_videos.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at http://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :booth_video do
-    title "MyString"
-    panda_video_id "MyString"
-  end
-end
