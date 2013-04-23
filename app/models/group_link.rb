9d9eb692200023711d0c7a593a697a98ea62dcfe
scottj <scottj@e05bbbe3-11a3-40ec-a18c-f20a9e1084ec>
2013-04-23 15:21:09 +0000

    Booth level changes - 3
    git-svn-id: http://cubeless.dev.sabre.com:18080/svn/cubeless/communities/agentstream@20267 e05bbbe3-11a3-40ec-a18c-f20a9e1084ec

--- app/models/group_link.rb
+++ app/models/group_link.rb
@@ -1,7 +1,9 @@
 class GroupLink < ActiveRecord::Base
 
-belongs_to :group
+  belongs_to :group
   validates_presence_of :group_id
-  validates_presence_of :url
-  validates_presence_of :text
+  validates_presence_of :url, :message => "We need a url to create a link"
+  validates_uniqueness_of :url, :message => "A link with this url already exists"
+  validates_presence_of :text, :message => "We need a text to create a link"
+  validates_uniqueness_of :text, :message => "A link with this text already exists"
 end
---
+++ spec/factories/booth_videos.rb
@@ -0,0 +1,9 @@
+# Read about factories at http://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :booth_video do
+    title "MyString"
+    panda_video_id "MyString"
+    group_id ""
+  end
+end
