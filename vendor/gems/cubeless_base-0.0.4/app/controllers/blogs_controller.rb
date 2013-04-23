9ed3a0f4be9046cc5619ffaaa9a6cb8aec54f503
scottj <scottj@e05bbbe3-11a3-40ec-a18c-f20a9e1084ec>
2013-04-23 15:21:29 +0000

    Booth level changes 6
    git-svn-id: http://cubeless.dev.sabre.com:18080/svn/cubeless/communities/agentstream@20276 e05bbbe3-11a3-40ec-a18c-f20a9e1084ec

--- vendor/gems/cubeless_base-0.0.4/app/controllers/blogs_controller.rb
+++ vendor/gems/cubeless_base-0.0.4/app/controllers/blogs_controller.rb
@@ -34,9 +34,11 @@ class BlogsController < ApplicationController
      max_id = Group.count_by_sql("select min(profile_id) from (select profile_id from group_memberships where group_id = #{@group.id} order by profile_id desc limit 200) as x")
     @booth_members = @group.members.all(:conditions => "profiles.id >= #{rand(max_id)+1}", :limit => 20).to_a.sort! { |a,b| rand(3)-1 }
     @group_blog_tags=TagCloud.tagcloudize(@group.blog.booth_tags.map{|x|x.name + " "})
-    @group_blog_tags.sort!{|a,b|a[:count]<=>b[:count]}
-    @minTagOccurs=@group_blog_tags.first[:count]
-    @maxTagOccurs=@group_blog_tags.last[:count]
+    if @group_blog_tags.count > 0
+      @group_blog_tags.sort!{|a,b|a[:count]<=>b[:count]}
+      @minTagOccurs=@group_blog_tags.first[:count]
+      @maxTagOccurs=@group_blog_tags.last[:count]
+    end
   end
 
 end
