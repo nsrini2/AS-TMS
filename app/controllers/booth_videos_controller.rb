cc055c17c77df01d048c7bd6c33df78e750a8e7d
scottj <scottj@e05bbbe3-11a3-40ec-a18c-f20a9e1084ec>
2013-04-23 15:21:37 +0000

    Booth level changes -8
    git-svn-id: http://cubeless.dev.sabre.com:18080/svn/cubeless/communities/agentstream@20278 e05bbbe3-11a3-40ec-a18c-f20a9e1084ec

--- app/controllers/booth_videos_controller.rb
+++ app/controllers/booth_videos_controller.rb
@@ -1,8 +1,9 @@
 class BoothVideosController < ApplicationController
+before_filter :find_booth_details
 
   def new
     @booth_video = BoothVideo.new
-    #render :layout => 'sponsored_group_manage_sub_menu'
+    render :layout => 'sponsored_group_manage_sub_menu'
   end
 
   def create
@@ -12,8 +13,7 @@ class BoothVideosController < ApplicationController
        render :action => "new"
     else
        @booth_video.marketing_video=MarketingVideo.new(:uploaded_data => params[:marketing_video_file])
-       Rails.logger.info("Niranjana Rahul - #{@booth_video.marketing_video.public_filename}")
-       #@video.convert
+       @booth_video.convert
        #flash[:notice] = 'Video has been uploaded'
        #redirect_to :action => 'show'
        #else
@@ -31,5 +31,18 @@ class BoothVideosController < ApplicationController
     @video.destroy
     redirect_to :action => 'show'
   end
+
+  def find_booth_details
+    @group = Group.find(params[:group_id])
+    @group_links = @group.group_links.all
+     max_id = Group.count_by_sql("select min(profile_id) from (select profile_id from group_memberships where group_id = #{@group.id} order by profile_id desc limit 200) as x")
+    @booth_members = @group.members.all(:conditions => "profiles.id >= #{rand(max_id)+1}", :limit => 20).to_a.sort! { |a,b| rand(3)-1 }
+    @group_blog_tags=TagCloud.tagcloudize(@group.blog.booth_tags.map{|x|x.name + " "})
+    if @group_blog_tags.count > 0
+      @group_blog_tags.sort!{|a,b|a[:count]<=>b[:count]}
+      @minTagOccurs=@group_blog_tags.first[:count]
+      @maxTagOccurs=@group_blog_tags.last[:count]
+    end
+   end
 end
 
